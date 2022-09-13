9db617453e2775a92d4a287a3261ce6402a1f92f
Arust <90620456+Arust-INSYS@users.noreply.github.com>
2022-09-13 00:50:52 -0500

    Creación Spring prueba

---
+++ proyectotfc/src/main/java/com/ista/proyectotfc/ProyectotfcApplication.java
@@ -0,0 +1,13 @@
+package com.ista.proyectotfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectotfcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectotfcApplication.class, args);
+	}
+
+}
---
+++ proyectotfc/src/test/java/com/ista/proyectotfc/ProyectotfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ista.proyectotfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectotfcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
